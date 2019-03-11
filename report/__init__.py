ddbee9d22fb8ed2b61d1b20fa536f2464d3a0090
Troy Larson <troylar@gmail.com>
2019-03-10 18:34:36 -0700

    Add numbering to resources

--- report/__init__.py
+++ report/__init__.py
@@ -9,25 +9,27 @@ class ReportGenerator:
         self.report_file = kwargs.get('ReportFile')
         self.tokens = {}
         self.data = {}
-        self.report_folder = kwargs.get('ReportFolder', 'report')
+        self.report_folder = kwargs.get('ReportFolder', 'report_ouput')
         if not os.path.exists(self.report_folder):
             os.makedirs(self.report_folder)
+            os.makedirs('{}/images'.format(self.report_folder))
 
     def import_data(self):
+        i = 1
         with open(self.report_file, "r") as f:
             for line in f:
                 m = re.match("^(.*):$", line)
                 if m:
                     res = m.groups()[0]
                     self.data[res] = {}
+                    self.create_icon(str(i))
+                    i = i + 1
                 else:
                     m = re.match('(.*)\s=\s(.*)$', line)
                     if m:
                         key = m.groups()[0].strip()
                         val = m.groups()[1].strip()
                         self.data[res][key] = val
-#                        if key == 'id':
-#                            self.create_icon(val)
                         if key in ('id', 'name'):
                             self.add_token(res, val)
                     else:
@@ -51,12 +53,12 @@ class ReportGenerator:
         self.tokens[key].append(token)
 
     def create_icon(self, id):
-        font = ImageFont.truetype('OpenSans-Bold.ttf', 10)
+        font = ImageFont.truetype('OpenSans-Bold.ttf', 12)
         w, h = font.getsize(id)
         width, height = (w+20, 25)
         img = Image.new("RGBA", (width, height), (255, 0, 0, 0))
         draw = ImageDraw.Draw(img)
         draw.rectangle((0, 0, width, height), fill="black")
         msg = id
-        draw.text(((width-w)/2, (height-h)/2), msg, font=font)
-        img.save('{}/{}.png'.format(self.report_folder, id), "PNG")
+        draw.text(((width-w)/2, ((height-h)/2)-2), msg, font=font)
+        img.save('{}/images/{}.png'.format(self.report_folder, id), "PNG")
